287a345fa67ca21ceb02dc978acd70bad6a9aed8
EndersonPro <enjaviga@gmail.com>
2018-10-21 23:28:52 -0500

    First Deploy Heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 app.use(express.static(__dirname + "/dist/"));
 
 app.get(/.*/, (req, res) => {
-    res.sendfile(__dirname + "/dist/index.html")
+    res.sendFile(__dirname + "dist/index.html")
 })
 
 app.listen(port, ()=> console.log("Servidor corriendo en puerto ", port))
